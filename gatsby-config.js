929b594d31e77f075c621a6c88cfc8b2d23fac7a
wcmohr <65930053+wcmohr@users.noreply.github.com>
2023-03-29 09:05:40 -0700

    gatsby-config.js personalization with projects, acct links, and brand statement.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,76 +3,88 @@ module.exports = {
     // Site URL for when it goes live
     siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Will Mohr',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Will Mohr | Data Scientist`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `I am dedicated to delivering actionable solutions to problems through effective computation.`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    // author: `@rfitzio`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/wcmohr`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/willcmohr/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `The thrill of arriving at deeper understandings of methodologies as well as the sound application of tools to better grapple with the challenges inherent in making use of data inspires me to be continuously learning, applying, and better understanding computational techniques.  I come from an accounting background that has taught me valuable skills in project management.  I also benefit from statistical know-how honed through the successful completion of five Society Of Actuaries examinations.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
-        name: 'Devfolio',
+        name: 'China Study',
         description:
-          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+          'A deep dive into one of the most monumental health datasets ever compiled.',
+        link: 'https://github.com/wcmohr/ChinaStudy',
       },
       {
-        name: 'ChromeExtensionKit',
+        name: 'Tennis Predictions',
         description:
-          'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
-        link: 'https://chromeextensionkit.com/?ref=devfolio',
+          'Comparing the predictive ability of three metrics for player ability.',
+        link: 'https://github.com/wcmohr/TennisPredictions',
       },
       {
-        name: 'Another Cool Project',
+        name: 'Reddit Classification',
         description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+          'Classifying between the r/Science and r/Technology subreddits.',
+        link: 'https://github.com/wcmohr/RedditClassification',
       },
+      {
+        name: 'Ames, IA Housing Insights',
+        description:
+          'Determining actionable drivers of house value.',
+        link: 'https://github.com/wcmohr/AmesHousing',
+      },
+      {
+        name: 'College Entrance Exam Analysis',
+        description:
+          'Exploring the relationship between statwide average scores and participation rates.',
+        link: 'https://github.com/wcmohr/ScholasticExams',
+      }
     ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
-    experience: [
-      {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-    ],
+    // experience: [
+    //   {
+    //     name: 'Acme Corp',
+    //     description: 'Full-Stack Developer, February 2020 - Present',
+    //     link: 'https://github.com/RyanFitzgerald/devfolio',
+    //   },
+    //   {
+    //     name: 'Globex Corp',
+    //     description: 'Full-Stack Developer, December 2017 - February 2020',
+    //     link: 'https://github.com/RyanFitzgerald/devfolio',
+    //   },
+    //   {
+    //     name: 'Hooli',
+    //     description: 'Full-Stack Developer, May 2015 - December 2017',
+    //     link: 'https://github.com/RyanFitzgerald/devfolio',
+    //   },
+    // ],
     // Optional: List your skills, they must have `name` and `description`.
-    skills: [
-      {
-        name: 'Languages & Frameworks',
-        description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
-      },
-      {
-        name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
-      },
-      {
-        name: 'Other',
-        description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
-      },
-    ],
-  },
+  //   skills: [
+  //     {
+  //       name: 'Languages & Frameworks',
+  //       description:
+  //         'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+  //     },
+  //     {
+  //       name: 'Databases',
+  //       description: 'MongoDB, PostreSQL, MySQL',
+  //     },
+  //     {
+  //       name: 'Other',
+  //       description:
+  //         'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+  //     },
+  //   ],
+  // },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
