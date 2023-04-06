1ce9f9db4c8dc198c43b090076c35b9d71e721de
wcmohr <65930053+wcmohr@users.noreply.github.com>
2023-04-05 21:47:13 -0700

    url

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://master--delicate-griffin-060a86.netlify.app/`,
     // Your Name
     name: 'Will Mohr',
     // Main Site Title
